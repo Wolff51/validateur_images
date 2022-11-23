e1435535e50026a38c56a6a8b8603b32ed9473a6
wolff <wolff.pro@outlook.fr>
2022-11-23 22:30:52 +0100

    recuperation des images data, debut des fontions

--- src/Components/Validation/Validation.js
+++ src/Components/Validation/Validation.js
@@ -1,11 +1,17 @@
-
 import './Validation.css';
 
 function Validation() {
+const imagesA = require.context('../../data/A', true, /(\.txt|\.jpg)$/);
+const imagesB = require.context('../../data/B', true, /(\.txt|\.jpg)$/);
+
+let keyA = imagesA.keys()
+
+console.log(keyA)
+
 return (
 <div className="images">
  <p className="subtitle">Voulez-vous valider cette image ?</p>
-<img src="https://www.w3schools.com/w3css/img_lights.jpg" alt="Lights" width="100%" height="100%" />
+<img src={imagesA(keyA[0])} alt="Lights" width="100%" height="100%" />
 <button className="button is-normal is-rounded  is-success is-responsive">Valider</button>
 <button className="button is-normal is-rounded  is-danger is-responsive">Refuser</button>
 <br/>
